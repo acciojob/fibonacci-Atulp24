aae6e317418ae7a1ce675eb8c6ed22d5a654f5e3
Atulp24 <apk200209@gmail.com>
2024-10-04 17:22:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
-// your code here
+	if(num === 1) {
+		return 0;
+	}
+	else if(num === 2) {
+		return 1;
+	}
+	let a = 0, b = 1;
+	for(let i = 3; i<=num; i++) {
+		let c = a + b;
+		a = b;
+		b = c;
+	}
+	return b;
 }
 
 module.exports = fibonacci;
